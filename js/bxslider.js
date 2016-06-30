8f9ec060a9fd4359727367b3b3a83ff57840424f
Stanislav Sapov <stanislav.sapov@meltwater.com>
2016-07-01 00:25:53 +0200

    change references to https

---
+++ js/bxslider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.bxslider').bxSlider({
+        auto: false,
+        speed: 400,
+        adaptiveHeight: true,
+        captions: true,
+        mode: 'fade',
+        infiniteLoop: true,
+        pager: false
+    });
+});
